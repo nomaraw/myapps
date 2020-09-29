0b4c93004dc42a69c5c7323d58cda136cf591844
nmarana <nomar_marana@optum.com>
2020-09-29 19:56:58 +0800

    commit23

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,4 @@
-console.log("update 22 nomar");
+console.log("update 23 nomar");
 
 (function ($, window, document, undefined) {
 
@@ -307,8 +307,9 @@ window.parent.openGPP = function() {
         var ezcommButtonVar = setInterval(addEzcommCoreLauncherGPPPayment, 1500);
         console.log('afterButtonVar');
         function addEzcommCoreLauncherGPPPayment() {
-            if (window.parent.$('iframe[id=' + activeTier1IframeId + ']').contents().find("span:contains('None of the cases found are related to the current inquiry')").length > 0 &&
-                window.parent.$('iframe[id=' + activeTier1IframeId + ']').contents().find("#gpppaymentheader").length === 0) {
+            //window.parent.$('iframe[id=' + activeTier1IframeId + ']').contents().find("span:contains('None of the cases found are related to the current inquiry')").length > 0 &&
+                
+            if (window.parent.$('iframe[id=' + activeTier1IframeId + ']').contents().find("#gpppaymentheader").length === 0) {
                     console.log('condition in');
                     $('#RULE_KEY > div:nth-child(1) > div > div > div > div > p').append('<button style="margin-bottom:10px" onclick="window.parent.openGPP()" type="button" id="gpppaymentheader"><div class="pzbtn-rnd" ><div class="pzbtn-lft"><div class="pzbtn-rgt" ><div class="pzbtn-mid" ><img src="webwb/zblankimage.gif" alt="" class="pzbtn-i">EZComm</div></div></div></div></button>');
                     }    
