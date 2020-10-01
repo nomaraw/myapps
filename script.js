a84099f7524dd851a9c8493cae9495689e81474c
nmarana <nomar_marana@optum.com>
2020-10-02 00:06:11 +0800

    Updates

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("update 3 nomar autodocrefreshing");
+console.log("update 1 nomar autodocrefreshing");
 
 (function ($, window, document, undefined) {
 
@@ -19,8 +19,37 @@ console.log("update 3 nomar autodocrefreshing");
 
     householdIdGpp = window.parent.$('iframe[id=' + activeTier1IframeId + ']')[0].contentWindow.getAttributeValue("pyWorkPage", "MemberID");
 
+
+    // function isAutodocMnrNotEmpty() {
+    //     if (sessionStorage.getItem('autodocmnrgpp') !== null) {
+    //         window.parent.sessionStorage.removeItem('autodocmnrgpp');
+    //         window.parent.sessionStorage.removeItem('messageSuccess');
+
+    //     } else if(sessionStorage.getItem('autodocmnrgpp') === null) {
+    //         window.parent.sessionStorage.removeItem('autodocmnrgpp');
+    //         window.parent.sessionStorage.removeItem('messageSuccess');
+
+    //     }
+    //     return false;
+    // }
+
+    // function checkIfReset(){
+    //     if(sessionStorage.getItem('autodocmnrgpp') !== null) {
+    //         window.parent.sessionStorage.removeItem('autodocmnrgpp');
+    //         window.parent.sessionStorage.removeItem('messageSuccess');
+    //         reset = true;
+    //     }
+    // }
+
+    // //TODO: remove?
+    // if (pageUrl == "MakeAPayment_GPSCC") {
+    //     isAutodocMnrNotEmpty();
+    // }
+
     function launchWinMnR() {
         var appWindow = window.parent.open("/a4me/ezcomm-core-v2/", "a4meEZCommWindow", 'location=no,height=600,width=1000,scrollbars=1');
+       // isAutodocMnrNotEmpty();
+       // checkIfReset();
 
         var configappt = false;
         var myObj = requestMetaDataGPP().plugins;
@@ -136,8 +165,9 @@ console.log("update 3 nomar autodocrefreshing");
     if(document.forms[0].elements["TaskSectionReference"] !== undefined) {
        
     if (document.forms[0].elements["TaskSectionReference"].value == "Tier1CompletionDetails") {
+        console.log('here tier1 autodoc')
         if (sessionStorage.getItem('autodocmnrgpp') !== null) {
-            sessionStorage.setItem('tier1GppAutoDocEzcomm', sessionStorage.getItem('autodocmnrgpp'));
+            sessionStorage.setItem('tier1GppGanyan', sessionStorage.getItem('autodocmnrgpp'));
             sessionStorage.removeItem('autodocmnrgpp');
         }
 
@@ -155,10 +185,20 @@ console.log("update 3 nomar autodocrefreshing");
             });
 
             if (configuration) {
-                if (sessionStorage.getItem('tier1GppAutoDocEzcomm') !== null) { // TODO: Storage name
-                    providerTierNotes = sessionStorage.getItem('tier1GppAutoDocEzcomm');
+                if (sessionStorage.getItem('tier1GppGanyan') !== null) { // TODO: Storage name
+                    providerTierNotes = sessionStorage.getItem('tier1GppGanyan');
+
+                    // if(sessionStorage.getItem('QuestionRadioStatusAppt') === "OPT_IN"  ) { // // TODO: Storage name
+                    //     sessionStorage.removeItem('QuestionRadioStatusAppt');
+                    //     sessionStorage.removeItem('schedproviders');
+                    // }
                 }                
-            } 
+            }  else {
+                // if(sessionStorage.getItem('QuestionRadioStatusAppt') === "OPT_IN" || sessionStorage.getItem('QuestionRadioStatusAppt') === "OPT_OUT") {
+                //     sessionStorage.removeItem('QuestionRadioStatusAppt');
+                //     sessionStorage.removeItem('schedproviders');
+               // }
+            }
             window.parent.$('iframe[id=' + activeTier1IframeId + ']').contents().find('#Comments').val(providerTierNotes);
         }
     }
@@ -272,8 +312,11 @@ window.parent.openGPP = function() {
         window.parent.addEventListener("message", messageEventGpp, false);
     }
 
+       console.log('beforeButtonVar');
         var ezcommButtonVar = setInterval(addEzcommCoreLauncherGPPPayment, 1500);
+        console.log('afterButtonVar');
         function addEzcommCoreLauncherGPPPayment() {
+            console.log('interval triggered');
             if (window.parent.$('iframe[id=' + activeTier1IframeId + ']').contents().find("span:contains('None of the cases found are related to the current inquiry')").length > 0 &&
                 window.parent.$('iframe[id=' + activeTier1IframeId + ']').contents().find("#gpppaymentheader").length === 0) {
                     console.log('condition in');
